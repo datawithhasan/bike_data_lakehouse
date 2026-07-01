9ce874133af373a587cf73e6225ff318f3f6abb8
datawithhasan <mdsiam76@gmail.com>
2026-07-01 09:29:43 +0000

    Completed core workflow steps
    Bronze(Data upload) --> Silver(Transformation) --> Gold(Business ready data)

---
+++ config/bronze_config.py
@@ -0,0 +1,35 @@
+base_dir = '/Volumes/workspace/bronze/source_sytem'
+ingestion_config = [
+    # CRM
+    {
+    "source": "crm",
+    "path": f"{base_dir}/source_crm/cust_info.csv",
+    "table": "crm_cust_info_raw"
+    },
+    {
+    "source": "crm",
+    "path": f"{base_dir}/source_crm/prd_info.csv",
+    "table": "crm_prd_info_raw"
+    },
+    {
+    "source": "crm",
+    "path": f"{base_dir}/source_crm/sales_details.csv",
+    "table": "crm_sales_details_raw"
+    },
+    # ERP
+    {
+    "source": "erp",
+    "path": f"{base_dir}/source_erp/CUST_AZ12.csv",
+    "table": "erp_cust_az12_raw"
+    },
+    {
+    "source": "erp",
+    "path": f"{base_dir}/source_erp/LOC_A101.csv",
+    "table": "erp_loc_a101_raw"
+    },
+    {
+    "source": "erp",
+    "path": f"{base_dir}/source_erp/PX_CAT_G1V2.csv",
+    "table": "erp_px_cat_g1v2"
+    }
+]
